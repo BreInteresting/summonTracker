7bf2203d5bee1e5f048dc1c9f930fa97883fe7e7
BreInteresting <129799677+BreInteresting@users.noreply.github.com>
2024-03-10 16:24:20 -0400

    Update SummonsCompaionTracker.py
    update to add background image

--- SummonsCompaionTracker.py
+++ SummonsCompaionTracker.py
@@ -10,22 +10,32 @@ Version 1.0
 '''
 import tkinter as tk
 from tkinter import messagebox, simpledialog, filedialog
-import random
+import tkinter as tk
+from PIL import ImageTk, Image
 from summonSheet import summon
 
+#main function to run the program
 def main():
+    # makes the main menu window
     global mainWindow
-    
     mainWindow = tk.Tk()
     mainWindow.title("Summon Creator")
-    mainWindow.geometry("250x120")
-    mainWindow.configure(background="gray")
-
-    summonButton = tk.Button(mainWindow, text="New Summon", bg="Red", fg="white", font=('arial', 20), command=summon)
-    summonButton.grid(row=4, column=2, padx=30, pady=30)
-
-    #loadButton = tk.Button(mainWindow, text="Load", command=loadButtonClick)
-    #loadButton.grid(row=5, column=2, padx=20, pady=30)
+    mainWindow.geometry("675x703")
+    # Load and display the background image on the main window
+    imagePath = "mainWindowBackground.jpg"
+    img = Image.open(imagePath)
+    backgroundImg = ImageTk.PhotoImage(img)
+    backgroundLabel = tk.Label(mainWindow, image=backgroundImg)
+    backgroundLabel.place(x=0, y=0, relwidth=1, relheight=1)
+    # a frame around the text. just for looks
+    frame = tk.Frame(mainWindow, bd=2, relief="solid", bg="black")
+    frame.place(x=160, y=100, width=400, height=200)
+    # the test on the window and placement of it 
+    mainWindowLabel = tk.Label(mainWindow, text="Do you need allies?\nNo one to help?\nSummon a Companion to help", fg="black", font=("Comic Sans MS", 20), bg="#D2B48C", highlightthickness=0)
+    mainWindowLabel.place(x=360, y=200, anchor="center")
+    # this is the button that opens the summon window. you can push it multiple times and get as many windows as you like
+    summonButton = tk.Button(mainWindow, text="New Summon", bg="Red", fg="white", font=('Comic Sans MS', 20), command=summon)
+    summonButton.place(x=260, y=400)
 
     mainWindow.mainloop()
 
